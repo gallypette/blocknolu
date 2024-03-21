8ea50a433ffa1512be45e564511dcac842f7c483
Jean-Louis Huynen <huynenjl@gmail.com>
2024-03-21 16:51:11 +0100

    add: [src] abiltiy to change country code

--- build_pf_list.py
+++ build_pf_list.py
@@ -1,9 +1,8 @@
 import requests
+import click
 
-# URL to fetch Luxembourg IP address ranges
-url = "https://raw.githubusercontent.com/ipverse/rir-ip/master/country/lu/aggregated.json"
 
-def fetch_luxembourg_ip_ranges(url):
+def fetch_ip_ranges(url):
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()
@@ -11,7 +10,7 @@ def fetch_luxembourg_ip_ranges(url):
         print("Failed to fetch data. Status code:", response.status_code)
         return None
 
-def generate_pf_rules(ip_ranges):
+def generate_pf_rules(ip_ranges, country):
     pf_rules = "table <luxembourg_ips> persist\n"
     
     if ip_ranges:
@@ -19,7 +18,7 @@ def generate_pf_rules(ip_ranges):
         ipv4_addresses = ip_ranges["subnets"]["ipv4"]
         ipv6_addresses = ip_ranges["subnets"]["ipv6"]
         
-        pf_rules = "table <luxembourg_ips> const {"
+        pf_rules = f"table <{country}_ips> const {{"
         
         for i, ip in enumerate(ipv4_addresses + ipv6_addresses):
             if i+1 != len(ipv4_addresses + ipv6_addresses):
@@ -32,18 +31,25 @@ def generate_pf_rules(ip_ranges):
 
         
         # Block all incoming connections by default and allow from Luxembourg IPs
-        pf_rules += """
+        pf_rules += f"""
 block in
 pass out
-pass in from <luxembourg_ips> to any
+pass in from <{country}_ips> to any
 """
     else:
         pf_rules = "Failed to generate PF rules due to data fetching error."
     
     return pf_rules
 
-# Fetch IP ranges and generate PF rules
-ip_ranges = fetch_luxembourg_ip_ranges(url)
-pf_rules = generate_pf_rules(ip_ranges)
 
-print(pf_rules)
+@click.command()
+@click.option('--country', required=True, type=click.STRING , help='Specify the country codes for which you want to allow access.')
+def cli(country):
+    url = f"https://raw.githubusercontent.com/ipverse/rir-ip/master/country/{country}/aggregated.json"
+    ip_ranges = fetch_ip_ranges(url)
+    pf_rules = generate_pf_rules(ip_ranges, country)
+    print(pf_rules)
+
+
+if __name__ == '__main__':
+    cli()
