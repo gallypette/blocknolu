11fec564af855548a90a7613ba414b96a3420ae2
William Robinet <william.robinet@conostix.com>
2024-03-22 10:29:54 +0100

    Invite some more friends to the party

--- build_geo_table.py
+++ build_geo_table.py
@@ -3,11 +3,13 @@ import click
 
 benelux = ["lu", "be", "nl"]
 eu = ["at", "be", "bg", "hr", "cy", "cz", "dk", "ee", "fi", "fr", "de", "gr", "hu", "ie", "it", "lv" ,"lt", "lu", "mt", "nl", "pl", "pt", "ro", "sk", "si", "es", "se"] 
+eu_ext = ["ad", "at", "ax", "be", "bg", "cy", "cz", "de", "dk", "ee", "es", "fi", "fr", "gb", "gi", "gr", "hr", "hu", "ie", "it", "li", "lt", "lu", "lv", "mc", "mt", "nl", "pl", "pt", "ro", "se", "si", "sk", "va"]
 country_list = ["ad", "ae", "af", "ag", "ai", "al", "am", "ao", "ar", "as", "at", "au", "aw", "ax", "az", "ba", "bb", "bd", "be", "bf", "bg", "bh", "bi", "bj", "bl", "bm", "bn", "bo", "bq", "br", "bs", "bt", "bw", "by", "bz", "ca", "cd", "cf", "cg", "ch", "ci", "ck", "cl", "cm", "cn", "co", "cr", "cu", "cv", "cw", "cy", "cz", "de", "dj", "dk", "dm", "do", "dz", "ec", "ee", "eg", "er", "es", "et", "eu", "fi", "fj", "fk", "fm", "fo", "fr", "ga", "gb", "gd", "ge", "gf", "gg", "gh", "gi", "gl", "gm", "gn", "gp", "gq", "gr", "gt", "gu", "gw", "gy", "hk", "hn", "hr", "ht", "hu", "id", "ie", "il", "im", "in", "io", "iq", "ir", "is", "it", "je", "jm", "jo", "jp", "ke", "kg", "kh", "ki", "km", "kn", "kp", "kr", "kw", "ky", "kz", "la", "lb", "lc", "li", "lk", "lr", "ls", "lt", "lu", "lv", "ly", "ma", "mc", "md", "me", "mf", "mg", "mh", "mk", "ml", "mm", "mn", "mo", "mp", "mq", "mr", "ms", "mt", "mu", "mv", "mw", "mx", "my", "mz", "na", "nc", "ne", "nf", "ng", "ni", "nl", "no", "np", "nr", "nu", "nz", "om", "pa", "pe", "pf", "pg", "ph", "pk", "pl", "pm", "pr", "ps", "pt", "pw", "py", "qa", "re", "ro", "rs", "ru", "rw", "sa", "sb", "sc", "sd", "se", "sg", "si", "sk", "sl", "sm", "sn", "so", "sr", "ss", "st", "sv", "sx", "sy", "sz", "tc", "td", "tg", "th", "tj", "tk", "tl", "tm", "tn", "to", "tr", "tt", "tv", "tw", "tz", "ua", "ug", "us", "uy", "uz", "va", "vc", "ve", "vg", "vi", "vn", "vu", "wf", "ws", "ye", "yt", "za", "zm", "zw"]
 
 country_groups = {
     "benelux": benelux,
     "eu": eu,
+    "eu_extended": eu_ext,
 }
 
 def fetch_ip_services(country):
@@ -65,7 +67,7 @@ def generate_tables_for_group(group, output_format):
 
 
 @click.command()
-@click.option('--country', required=True, type=click.Choice(['eu', 'benelux'] + country_list, case_sensitive=False), help='Specify the country code for which you want to create a table, or benelux, or eu.')
+@click.option('--country', required=True, type=click.Choice(['eu_extended', 'eu', 'benelux'] + country_list, case_sensitive=False), help='Specify the country code for which you want to create a table, or benelux, or eu.')
 @click.option('--format', type=click.Choice(['pf', 'cisco'], case_sensitive=False), default='pf', help='Specify the format of the output. Default is pf.')
 def cli(country, format):
     if country in country_groups:
